0b0910cd91cad47a86c44563981290f49712dbf6
japps1a19 <japps1a19@cicct.local>
2017-08-17 11:43:36 +0800

    code Update
    lalalallalalallaa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+studentCollection:Array<object>=[];
+studentRecord:object;
+studNo:number;
+studFname:string;
+studLname:string;
+studProg:string;
+studYr:number;
+
+messages='';
+printing=false;
+
+private checkPatterns(value:any,pattern:RegExp):boolean{
+  if(pattern.test(value))
+    return true;
+else 
+  return false;
+}
+addStudent():boolean{
+this.printing=false;
+const stringPattern=/^a-z\s]+$/;
+const studNumberPattern=/^[0-9]+$/;
+const studYearPattern=/^[1-5]+$/;
+if (this.checkPatterns(this.studNo,studNumberPattern)&&(this.studFname,stringPattern)&&(this.studLname,stringPattern)&&(this.studProg,stringPattern)&&(this.studYr,studYearPattern)){
+this.studentRecord={
+  studNumber:this.studNo,
+  studFirstName:this.studFname,
+  studLastName:this.studLname,
+  studProgram:this.studProg,
+  studYear:this.studYr
+};
+
+this.studentCollection.push(this.studentRecord);
+this.messages=null;
+this.clearValues();
+}else{
+  this.messages='Errors have been encountered and therefore cannot continue to process requested operation';
+  return false;
+}
+}
+listofStudent():void{
+  this.printing=true;
+  console.log('Showing stored Students');
+}
+ clearValues():void{
+  this.studNo=null;
+  this.studFname=null;
+  this.studLname=null;
+  this.studProg=null;
+  this.studYr=null;
+  
+}
 }
